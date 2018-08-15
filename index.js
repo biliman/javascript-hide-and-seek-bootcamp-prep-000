6e2d831c7e4eeb19a3fa56de71c822a9a40e3aa8
Fickry Bil Iman <fickry.bil.iman@gmail.com>
2018-08-15 04:29:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  let arr = document.querySelectorAll('.ranked-list li');
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].innerHTML = +arr[i].innerHTML + n;
+  }
+  return arr;
+}
+
+function deepestChild() {
+  let arr = document.querySelector('#grand-node div');
+  return arr[arr.length - 1];
+}
